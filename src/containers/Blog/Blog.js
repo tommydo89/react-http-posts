17f33be6536d66625efcc18dae6994a4970c10a5
VISTAPRINTUS\jmaffeo <jmaffeo@vistaprint.com>
2019-01-08 18:27:54 -0500

    push small change that was not saved

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 /*import axios from '../../axios';*/
-import { Route , NavLink} from 'react-router-dom';
+import { Route , NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -32,9 +32,11 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
-                <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
-                <Route path="/posts/:id" exact component={FullPost}/>
+                <Switch>
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/posts/:id" exact component={FullPost}/>
+                </Switch>
             </div>
         );
     }
