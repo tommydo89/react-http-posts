56a55bd852c4a65b7991444200c02b0cdbebd353
Tommy Do <tommy.td89@gmail.com>
2019-01-13 11:02:36 -0500

    routes and etc.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 /*import axios from '../../axios';*/
-import { Route , NavLink, Switch } from 'react-router-dom';
+import { Route , NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPost from './NewPost/NewPost';
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 
 class Blog extends Component {
+
+    state = {
+        auth: true
+    }
+
     render () {
         return (
             <div className="Blog">
@@ -16,13 +24,13 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink 
-                                to="/" 
+                                to="/posts" 
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
                                     textDecoration: 'underline'
-                                }}>Home</NavLink></li>
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -33,9 +41,11 @@ class Blog extends Component {
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
                 <Switch>
-                    <Route path="/" exact component={Posts}/>
-                    <Route path="/new-post" component={NewPost}/>
-                    <Route path="/posts/:id" exact component={FullPost}/>
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
+                    <Route path="/posts" component={Posts}/>
+                    <Route render={() =><h1>Not found</h1>}/>
+                    {/*<Redirect from="/" to="/posts"/>*/}
+                    {/*<Route path="/" component={Posts}/>*/}
                 </Switch>
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+import { Route } from 'react-router-dom';
+
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-import { Link } from 'react-router-dom';
-
+import FullPost from '../FullPost/FullPost';
 class Posts extends Component {
 	
 	state = {
@@ -30,25 +31,32 @@ class Posts extends Component {
     }
 
 	postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
+		// this.props.history.push({pathname: '/posts/' + id})
+		this.props.history.push('/posts/' + id);
     }
 
 	render() {
 		let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
 		if (!this.state.error){
 		    posts = this.state.posts.map(post => {
-		    return <Link to={'/posts/' + post.id} key={post.id}>
-			    		<Post 
+		    return (
+		    	// <Link to={'/posts/' + post.id} key={post.id}>
+			    		<Post
+			    		key={post.id} 
 			            title={post.title} 
 			            author={post.author}
 			            clicked={() => this.postSelectedHandler(post.id)}/>
-			        </Link>
+			    // </Link>
+			    );
 		    });
 		}
 		return (
-			<section className="Posts">
+			<div>
+				<section className="Posts">
                 {posts}
-            </section>
+            	</section>
+            	<Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+			</div>
 		)
 	}
 }
